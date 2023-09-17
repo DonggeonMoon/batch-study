8350ab29616751d552a8d30919ee54d4d0f606aa
Donggeon Moon <nullsafer@naver.com>
2023-09-17 15:48:42 +0900

    chore: 실습에 필요한 util 클래스 등 추가

---
+++ src/test/java/com/batchstudy/testutils/CourseUtilsJsonData.java
@@ -0,0 +1,13 @@
+package com.batchstudy.testutils;
+
+@SuppressWarnings("WeakerAccess")
+public abstract class CourseUtilsJsonData {
+    public String value;
+
+    @Override
+    public String toString() {
+        return "Data{" +
+                "value='" + value + '\'' +
+                '}';
+    }
+}
