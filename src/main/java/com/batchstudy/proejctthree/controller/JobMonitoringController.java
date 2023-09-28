0629300de05ede7ebcfc4b00ec653e2b73425eb5
Donggeon Moon <nullsafer@naver.com>
2023-09-28 19:03:20 +0900

    feat: third project(running job visualization) 코드 추가

--- src/main/java/com/batchstudy/proejctthree/controller/JobMonitoringController.java
+++ src/main/java/com/batchstudy/proejctthree/controller/JobMonitoringController.java
@@ -1,10 +1,10 @@
 package com.batchstudy.proejctthree.controller;
 
-import com.batchstudy.proejctthree.config.SimpleJob;
+import com.batchstudy.proejctthree.dto.JobExecutionsDto;
 import com.batchstudy.proejctthree.dto.JsonWrapper;
 import com.batchstudy.proejctthree.services.TriggerJobService;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParametersInvalidException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.launch.JobExecutionNotRunningException;
@@ -16,12 +16,15 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "job-monitoring")
+@RequiredArgsConstructor
 public class JobMonitoringController {
 
+    private final JobRepository jobRepository;
     private final JobExplorer jobExplorer;
     private final TriggerJobService triggerJobService;
     private final List<Job> jobs;
@@ -29,58 +32,51 @@ public class JobMonitoringController {
     private final Job simpleJob;
     private final JobRegistry jobRegistry;
 
-    public JobMonitoringController(JobRepository jobRepository,
-                                   JobExplorer jobExplorer,
-                                   TriggerJobService triggerJobService,
-                                   List<Job> jobs, JobOperator jobOperator,
-                                   @SimpleJob Job simpleJob,
-                                   JobRegistry jobRegistry) {
-        this.jobExplorer = jobExplorer;
-        this.triggerJobService = triggerJobService;
-        this.jobs = jobs;
-        this.jobOperator = jobOperator;
-        this.simpleJob = simpleJob;
-        this.jobRegistry = jobRegistry;
-    }
-
     @GetMapping("/job-definitions")
     public JsonWrapper jobDefinitions() {
-        return new JsonWrapper(0);
+        return new JsonWrapper(jobs.size());
     }
 
     @GetMapping("/job-names")
     public JsonWrapper getJobNames() {
-        return new JsonWrapper(List.of());
+        Collection<String> jobNames = jobRegistry.getJobNames();
+        return new JsonWrapper(jobNames);
     }
 
     @PostMapping("/job/start-new")
     public void runJob(@RequestParam(value = "jobName") String jobName) throws NoSuchJobException {
-
+        Job job = jobRegistry.getJob(jobName);
+        triggerJobService.triggerJob(job);
     }
 
     @GetMapping("/job/{jobName}/instances")
     public JsonWrapper getJobInstances(@PathVariable String jobName) {
-        ;
-        return new JsonWrapper(List.of());
+        List<JobInstance> jobInstances = jobExplorer.getJobInstances(jobName, 0, 100);
+        return new JsonWrapper(jobInstances);
     }
 
     @GetMapping("/job/instance/{instanceId}")
     public JsonWrapper getJobExecutions(@PathVariable Long instanceId) {
-        return null;
+        JobInstance jobInstance = jobExplorer.getJobInstance(instanceId);
+        List<JobExecution> jobExecutions = jobExplorer.getJobExecutions(jobInstance);
+        return new JsonWrapper(new JobExecutionsDto(jobExecutions));
     }
 
     @PostMapping("/execution/{executionId}/stop")
     public void stopExecution(@PathVariable Long executionId) throws NoSuchJobExecutionException, JobExecutionNotRunningException {
-
+        jobOperator.stop(executionId);
     }
 
     @PostMapping("/execution/{executionId}/restart")
     public void startExecution(@PathVariable Long executionId) throws JobParametersInvalidException, JobRestartException, JobInstanceAlreadyCompleteException, NoSuchJobExecutionException, NoSuchJobException {
-
+        jobOperator.restart(executionId);
     }
 
     @PostMapping("/simple-job/start/{parameter}")
     public void startSimpleJobWithParameter(@PathVariable String parameter) {
-
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("parameter", parameter)
+                .toJobParameters();
+        triggerJobService.triggerJob(simpleJob, jobParameters);
     }
 }
